029da52c927ceeb4d1042333cb1ec013d294116e
VyacheslavAI <vyacheslavivanov93@gmail.com>
2019-02-15 19:10:25 +0300

    Replace JpaConfig implementation with xml for properties

--- server/src/main/java/ru/ivanov/todoproject/config/JpaConfig.java
+++ server/src/main/java/ru/ivanov/todoproject/config/JpaConfig.java
@@ -1,26 +1,62 @@
 package ru.ivanov.todoproject.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = "ru.ivanov.todoproject.repository")
+@PropertySource("classpath:application.properties")
 public class JpaConfig {
 
-    @Bean("entityManagerFactory")
-    public LocalEntityManagerFactoryBean entityManagerFactoryBean() {
-        final LocalEntityManagerFactoryBean factoryBean = new LocalEntityManagerFactoryBean();
-        factoryBean.setPersistenceUnitName("jpamanager");
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public DataSource dataSource(final Environment environment) {
+        final DriverManagerDataSource managerDataSource = new DriverManagerDataSource();
+        managerDataSource.setDriverClassName(environment.getRequiredProperty("db.driver"));
+        managerDataSource.setUrl(environment.getRequiredProperty("db.url"));
+        managerDataSource.setUsername(environment.getRequiredProperty("db.username"));
+        managerDataSource.setPassword(environment.getRequiredProperty("db.password"));
+        return managerDataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(final DataSource dataSource, final Environment environment) {
+        final LocalContainerEntityManagerFactoryBean factoryBean
+                = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        factoryBean.setPackagesToScan("ru.ivanov.todoproject");
+
+        final Properties jpaProperties = new Properties();
+        jpaProperties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        jpaProperties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        jpaProperties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+        jpaProperties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+
+        factoryBean.setJpaProperties(jpaProperties);
         return factoryBean;
     }
 
-    @Bean("transactionManager")
-    public JpaTransactionManager getJpaTransactionManager() {
-        return new JpaTransactionManager();
+    @Bean
+    public JpaTransactionManager transactionManager(final EntityManagerFactory entityManagerFactory) {
+        final JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
     }
 }
