b1c101d6368f245eebb0668bf0906a4501a3ee56
va.ivanov <vyacheslavivanov93@gmail.com>
2019-03-01 14:22:56 +0300

    Добавить класс для реализации spring security

---
+++ server/src/main/java/ru/ivanov/todoproject/webinitializer/SecurityWebInitializer.java
@@ -0,0 +1,6 @@
+package ru.ivanov.todoproject.webinitializer;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
+}
